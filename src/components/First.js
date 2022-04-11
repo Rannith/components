647c624eaff6a0762fd86822ad0f21ec574e5823
Rannith <kmrannithvikram451@gmail.com>
2022-04-11 11:24:10 +0530

    master

---
+++ src/components/First.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const First = () => <h1>This is component One</h1>
+
+export default First
---
+++ src/components/Second.js
@@ -0,0 +1,12 @@
+
+//Class 
+
+import React, { Component } from "react";
+
+class Second extends Component {
+    render() {
+        return <h2>This is Second Component</h2>
+    }
+}
+
+export default Second
